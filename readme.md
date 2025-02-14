f64d030173c63516aae12aef29494949b0dee45c
mjkushman <mjkushman@gmail.com>
2025-02-14 13:00:02 -0800

    completed this little chatbot

---
+++ readme.md
@@ -0,0 +1,35 @@
+# Simple Rag - /dev/color
+
+## Getting Started
+
+Create a .env file and add your secret key from OpenAI.
+
+```
+OPENAI_API_KEY=REPLACE-WITH-YOUR-SECRET-KEY
+```
+
+Install and start the application:
+
+```bash
+npm install
+npm start
+```
+
+## Usage
+
+```node
+Ask me anything:
+```
+
+Type your query into the command line:
+
+User input:
+> How can /dev/color help me develop my career?
+
+Two steps are taken:
+
+1. The app searchs a corpus for related Q&As.
+2. The app constructs a prompt for ChatGPT to answer the user's query, including any relevant Q&A as context if any are found.
+
+Bot output:
+>/dev/color can help you develop your career by providing a supportive community focused on advancing Black technologists. It offers mentorship, career guidance, and professional development through workshops and events. Partnering with companies and organizations in the tech industry, /dev/color facilitates networking opportunities that can connect you with potential employers and mentors, potentially opening new career paths and opportunities.
---
+++ src/index.ts
@@ -0,0 +1,76 @@
+import "dotenv/config";
+import readline from "readline";
+import OpenAI from "openai";
+import corpus from "./corpus.json";
+import Fuse, { FuseResult } from "fuse.js";
+
+// process.exit()
+
+// Initialize OpenAI with API Key
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+
+// Set up fuzzy search
+const fuse = new Fuse(corpus, {
+  keys: ["question"],
+  minMatchCharLength: 3,
+});
+
+// Uses fuzzy search RAG to find relevant questions and answers for context.
+function getContext(query: string): FuseResult<(typeof corpus)[0]>[] {
+  let context = fuse.search(query);
+  return context;
+}
+
+// Function to generate a response using OpenAI
+async function generateResponse(query: string): Promise<string> {
+  const context = getContext(query);
+
+  let messages: { role: "system" | "user"; content: string }[] = [
+    {
+      role: "system",
+      content: `Provide a succinct answer to the user's qustion.`,
+    },
+  ];
+
+  // If there is relevant context, add it to the prompt
+  if (context.length > 0) {
+    let contextString = `Use these question + answer pairs to help generate a response: \n\n`;
+    for (let item of context) {
+      contextString += `Q: ${item.item.question} \n A: ${item.item.answer} \n\n`;
+    }
+    messages.push({ role: "system", content: contextString });
+  }
+
+  // Add the user's query to the prompt
+  messages.push({ role: "user", content: query });
+
+  const response = await openai.chat.completions.create({
+    model: "gpt-4o",
+    messages: [...messages],
+  });
+  return (
+    response.choices[0]?.message?.content || "Error: No response generated."
+  );
+}
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: "Ask me anything: ",
+});
+
+// Chat
+async function chat() {
+  console.log("Type 'exit' any time to quit.", "\n");
+  rl.prompt();
+
+  rl.on("line", async (input) => {
+    if (input.toLowerCase() === "exit") process.exit(0);
+    console.log("Thinking..."); // optional message
+    const response = await generateResponse(input);
+    console.log(response);
+    rl.prompt();
+  });
+}
+
+chat();
